86f93315901e53f14fe75c8b3612112da915ecb1
Vitaliy <vbnyak@gmail.com>
2024-03-18 09:35:59 +0200

    first commit

---
+++ Car.java
@@ -0,0 +1,21 @@
+public class Car {
+    private void start()
+    {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    public void startElectricity()
+    {
+
+    }
+    public void startCommand()
+    {
+
+    }
+    public void startFuelSystem()
+    {
+
+    }
+}
